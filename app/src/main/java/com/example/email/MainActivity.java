0f7760ea7a6eda73d1803016c1aba477a75885f0
Aqsa <aqsa.isha2002@gmail.com>
2023-05-22 17:38:18 +0500

    finished project

---
+++ app/src/main/java/com/example/email/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.email;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.net.Uri;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Email App (20SW27)");
+        Button startBtn = (Button) findViewById(R.id.sendbttn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                sendEmail();
+            }
+        });
+    }
+    protected void sendEmail() {
+        Log.i("Send email", "");
+        String[] TO = {"aqsa.isha2002@gmail.com"};
+        String[] CC = {"soomroaqsa337@gmail.com"};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("Finished sending email...", "");
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(MainActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
